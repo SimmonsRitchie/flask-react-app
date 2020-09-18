06b98def18434c36ac681aa67f4d8004d6acafc5
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-18 06:53:27 -0400

    Config wsgi

--- api/api.py
+++ api/api.py
@@ -10,4 +10,4 @@ def index():
 
 
 # if __name__ == "__main__":
-#     app.run(host="127.0.0.1", debug=True, port=os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", debug=True, port=os.environ.get("PORT", 80))
