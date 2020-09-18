c747de20350c2973f947788a07c87098938719fb
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-17 21:31:06 -0400

    Fix build issues

--- api/api.py
+++ api/api.py
@@ -1,11 +1,13 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_folder='../dist', static_url_path='/')
+app = Flask(__name__, static_folder="../dist", static_url_path="/")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(host="127.0.0.1", debug=True, port=os.environ.get("PORT", 5000))
