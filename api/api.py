8f0b71cd5541d9e40c3cf90bf35ca342265e85de
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-17 20:42:16 -0400

    Prepare for deploy

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_folder='./dist', static_url_path='/')
+app = Flask(__name__, static_folder='../dist', static_url_path='/')
 
 @app.route('/')
 def index():
