144d9085a87e9c6ac45ac482d10c9790300be8f8
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-19 14:19:10 -0400

    Strip down

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,23 @@ import React from "react";
 
 class Main extends React.Component {
   render() {
-    return <div className="container__outer">A blank app!</div>;
+    return (
+      <section className="container">
+        <div className="columns is-multiline">
+          <div className="column is-8 is-offset-2 register">
+            <h1 className="title is-1">Flask-React App</h1>
+            <h2 className="subtitle colored is-4">
+              Boilerplate app that uses Flask for the backend, React for the
+              frontend.
+            </h2>
+
+            <button type="button" className="button">
+              Test!
+            </button>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
