e6c9eef82438bf313f1d2127e2cd1b1dcede073e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-19 10:35:31 -0400

    experimental

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_folder="../dist", static_url_path="/")
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="../dist", static_url_path="/")
 
 
 @app.route("/")
 def index():
-    return app.send_static_file("index.html")
+    return "<div>hello world?</div>"
+    # return app.send_static_file("index.html")
 
 
 # if __name__ == "__main__":
