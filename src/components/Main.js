248ccab9db342f2a2f716cbdf6eb11bee08f6f14
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-19 14:03:00 -0400

    Fix up scripts

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Main extends React.Component {
   render() {
-    return <div className="container__outer">Hello world!</div>;
+    return <div className="container__outer">A blank app!</div>;
   }
 }
 
