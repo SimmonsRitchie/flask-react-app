d29e53e2e98cdeecbb41c0d7818efe11a31bd370
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-20 15:03:59 -0400

    Tidy up

--- client/components/Main.js
+++ client/components/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
     <section className="container">
       <div className="columns is-multiline">
         <div className="column is-8 is-offset-2">
-          <h1 className="title is-1">Flask-React App!!!</h1>
+          <h1 className="title is-1">Flask-React App</h1>
           <h2 className="subtitle colored is-4">
             Boilerplate app that uses Flask for the backend, React for the
             frontend.
